6174e406e565ca9a82b14e87883147aed24074c0
Joelmir Ribacki <joedekid@gmail.com>
2015-07-23 00:09:11 -0300

    add questions on template

--- core/views.py
+++ core/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from questions.models import Question
 
 def home(request):
-  return render(request, 'index.html')
+
+  return render(request, 'index.html', { 
+      'questions':Question.objects.all().order_by('question_text')
+    })
