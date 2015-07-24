5c40938f3e4dcda14032ca7565f67503a9f525d0
Joelmir Ribacki <joedekid@gmail.com>
2015-07-24 00:36:03 -0300

    finalizado layout de cadastro

--- core/views.py
+++ core/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from questions.models import Question
+from django.core.mail import send_mail
 
 def home(request):
 
-  return render(request, 'index.html', { 
+    #send_mail('Subject here', 'Here is the message.', 'joelmir.ribacki@gmail.com',['joedekid@gmail.com'], fail_silently=False)
+    return render(request, 'index.html', { 
       'questions':Question.objects.all().order_by('question_text')
     })
